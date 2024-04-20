a387b92116ffa76fd1c4280435108a45d2c3a7ee
abedalawieh <abedalaweye@gmail.com>
2024-04-20 14:18:05 +0300

    Added the project to the repo

--- app/listings/[listingid]/page.tsx
+++ app/listings/[listingid]/page.tsx
@@ -2,7 +2,7 @@ import EmptyState from "@/app/components/EmptyState";
 import { singleList } from "@/app/components/examples/apartments";
 import ListingClient from "./ListingClient";
 const ListingPage = async () => {
-  if (!singleList) {
+  if (singleList) {
     return <EmptyState />;
   }
   return <ListingClient listing={singleList} />;
